829b710adcead5b48708807ae107b0348dd86301
gustavodecamposs <gustavoo.dcsilva@gmail.com>
2025-01-09 17:32:08 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let nome = "Bonieky" // string
+let idade = 90  // number
+let logado = true // boolean
+let lista = ["ovo", "massa", "corante"] // array
+let nomeCompleto = { nome: "Bonieky", idade: 90 }// object
+
+console.log(typeof logado)
+
+
+
