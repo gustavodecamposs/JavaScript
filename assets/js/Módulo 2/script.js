9a7e3961fff96fdfd4446134f6486043e2845d61
gustavodecamposs <gustavoo.dcsilva@gmail.com>
2025-01-31 12:12:31 -0300

    .

--- assets/js/Módulo 2/script.js
+++ assets/js/Módulo 2/script.js
@@ -1,2 +1,9 @@
 //- O que é uma função?
 
+function gravidade() {
+    console.log('A gravidade do planeta é:');
+    console.log(9.8);
+}
+
+console.log('Opa, tudo bem?')
+gravidade();
---
+++ assets/js/Módulo 2/script002.js
@@ -0,0 +1,14 @@
+// function somar(n1, n2) {
+//     let resultado = n1 + n2;
+//     console.log('Resultado: ' + resultado);
+// }
+
+// somar(10, 15)
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+    
+}
+
+let completo = nomeCompleto("Bonieky", "Lacerda");
+console.log('Completo: ' + completo);
