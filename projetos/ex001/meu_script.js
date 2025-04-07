990bd2497da8c7073f19860cd3cd35489d769a50
gustavodecamposs <gustavoo.dcsilva@gmail.com>
2025-04-07 11:45:21 -0300

    Variáveis.

--- projetos/ex001/meu_script.js
+++ projetos/ex001/meu_script.js
@@ -1,2 +1,7 @@
 //alert('Olá, seja bem vido ao curso!!!')
+
+/*
+    selecionar um elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
 document.getElementById('nome').value = 'Oi'
